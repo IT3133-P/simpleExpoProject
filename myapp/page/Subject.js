254106808c78b465a2352a9dc022a5e87aa5d0f2
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2025-01-06 11:18:51 +0530

    feat: create subject page

---
+++ myapp/page/Subject.js
@@ -0,0 +1,47 @@
+import { Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import Header from "../com/Header";
+import Footer from "../com/Footer";
+
+export default function Subject() {
+
+    return (
+        <>
+            <View style={styles.box1}>
+                <Header/>
+            </View>
+            <View style={styles.box2}>
+                <Text>Subject</Text>
+            </View>
+            <View style={styles.box3}>
+                <Footer/>
+            </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    box1: {
+        flex: 1,
+        backgroundColor: 'green',
+        width:'100%'
+    },
+    box2: {
+        flex: 4,
+        backgroundColor: 'white',
+        width:'100%'
+    },
+    box3: {
+        flex: 1,
+        backgroundColor: 'blue',
+        width:'100%'
+    }
+})
+
+
